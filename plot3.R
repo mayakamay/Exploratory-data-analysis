e910a0b1d097980e3b88bad1b32b380aeac341fd
mayakamay <35781399+mayakamay@users.noreply.github.com>
2020-12-22 15:23:10 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,7 +1,12 @@
-my_data$Date <- as.Date(my_data$Date)
-my_data$Datetime <- as.POSIXct(paste(my_data$Date,my_data$Time))
-png("plot2.png")
-plot(my_data$Datetime, as.numeric(my_data$Global_active_power), type='l',
+##Plot 3
+png('plot3.png')
+plot(my_data$Datetime, as.numeric(my_data$Sub_metering_1), type='l',
      xlab='',
-     ylab='Global Active Power (kilowatts)')
+     ylab='Energy sub metering')
+lines(my_data$Datetime,as.numeric(my_data$Sub_metering_2),type='l',col='red')
+lines(my_data$Datetime,as.numeric(my_data$Sub_metering_3),type='l',col='blue')
+legend("topright", 
+       legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
+       lty=1,
+       col=c("black","red","blue"))
 dev.off()
